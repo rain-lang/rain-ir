2335259bc3cebef5126b94288cd78802e7ab831d
Jad Ghalayini <jad.ghalayini@hotmail.com>
2020-05-31 17:20:49 -0400

    Builder building

--- src/parser/builder/mod.rs
+++ src/parser/builder/mod.rs
@@ -7,7 +7,7 @@ use ahash::RandomState;
 use std::fmt::{self, Debug, Formatter};
 use std::hash::{BuildHasher, Hash};
 
-/// A builder for `rain` expressions
+/// A rain IR builder
 pub struct Builder<S: Hash + Eq, B: BuildHasher = RandomState> {
     symbols: SymbolTable<S, ValId, B>,
 }
@@ -18,4 +18,26 @@ impl<S: Hash + Eq + Debug, B: BuildHasher> Debug for Builder<S, B> {
             .field("symbols", &self.symbols)
             .finish()
     }
+}
+
+
+impl<'a, S: Hash + Eq + From<&'a str>> Builder<S> {
+    /// Create a new builder
+    pub fn new() -> Builder<S> {
+        Builder {
+            symbols: SymbolTable::new()
+        }
+    }
+}
+
+impl<'a, S: Hash + Eq + From<&'a str>, B: BuildHasher + Default> Default for Builder<S, B> {
+    fn default() -> Builder<S, B> {
+        Builder {
+            symbols: SymbolTable::default()
+        }
+    }
+}
+
+impl<'a, S: Hash + Eq + From<&'a str>, B: BuildHasher> Builder<S, B> {
+    //TODO: build it!
 }
