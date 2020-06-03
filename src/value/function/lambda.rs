f05611b9b3e7743ae222822893c1bc410fb9ff30
Jad Ghalayini <jad.ghalayini@hotmail.com>
2020-06-03 02:55:59 -0400

    Started adding function types

--- src/value/function/lambda.rs
+++ src/value/function/lambda.rs
@@ -1,3 +1,16 @@
 /*!
 Lambda functions
 */
+/*
+use super::pi::Pi;
+use crate::value::{lifetime::Parametrized, ValId, VarId};
+use std::fmt::{self, Debug, Formatter};
+
+/// A lambda function
+pub struct Lambda {
+    /// The result of this lambda function
+    result: Parametrized<ValId>,
+    /// The type of this lambda function
+    ty: VarId<Pi>,
+}
+*/
--- src/value/function/pi.rs
+++ src/value/function/pi.rs
@@ -1,3 +1,13 @@
 /*!
 Pi types
 */
+use crate::value::{lifetime::Parametrized, TypeId, UniverseId};
+
+/// A pi type
+#[derive(Debug)]
+pub struct Pi {
+    /// The result of this pi type
+    result: Parametrized<TypeId>,
+    /// The type of this pi type
+    ty: UniverseId
+}
