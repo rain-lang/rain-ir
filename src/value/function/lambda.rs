66edbdad0f178bd7c8a7c5f42b06a4c950d700d6
Jad Ghalayini <jad.ghalayini@hotmail.com>
2020-06-04 03:46:46 -0400

    Skeleton of Lambda

--- src/value/function/lambda.rs
+++ src/value/function/lambda.rs
@@ -1,16 +1,35 @@
 /*!
 Lambda functions
 */
-/*
+use crate::{debug_from_display, pretty_display};
 use super::pi::Pi;
 use crate::value::{lifetime::Parametrized, ValId, VarId};
-use std::fmt::{self, Debug, Formatter};
 
 /// A lambda function
+#[derive(Clone, Eq, PartialEq, Hash)]
 pub struct Lambda {
     /// The result of this lambda function
     result: Parametrized<ValId>,
     /// The type of this lambda function
     ty: VarId<Pi>,
 }
-*/
+
+debug_from_display!(Lambda);
+pretty_display!(Lambda, "#lambda |...| {...}");
+
+#[cfg(feature = "prettyprinter")]
+mod prettyprint_impl {
+    use super::*;
+    use crate::prettyprinter::{PrettyPrint, PrettyPrinter};
+    use std::fmt::{self, Display, Formatter};
+
+    impl PrettyPrint for Lambda {
+        fn prettyprint<I: From<usize> + Display>(
+            &self,
+            _printer: &mut PrettyPrinter<I>,
+            _fmt: &mut Formatter,
+        ) -> Result<(), fmt::Error> {
+            unimplemented!()
+        }
+    }
+}
