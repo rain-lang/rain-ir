d46eb13ddf070180ab9c1af1965d4e95596f321e
Jad Ghalayini <jad.ghalayini@hotmail.com>
2020-06-09 16:01:19 -0400

    Arraywork

--- src/value/arr.rs
+++ src/value/arr.rs
@@ -13,11 +13,11 @@ use triomphe::{Arc, HeaderSlice, HeaderWithLength, ThinArc};
 
 lazy_static! {
     /// A cache for arrays of values
-    pub static ref ARRAY_CACHE: Cache<ValId, ValArr> = Cache::default();
+    pub static ref ARRAY_CACHE: Cache<ValId, ArcValIdArr> = Cache::default();
 }
 
 /// A reference-counted, hash-consed, typed array of values
-#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+#[derive(Debug, Clone, Eq, PartialEq, Hash, RefCast)]
 #[repr(transparent)]
 pub struct VarArr<V> {
     arr: PrivateValArr,
@@ -35,11 +35,45 @@ impl<V> VarArr<V> {
     pub fn is_empty(&self) -> bool {
         self.arr.is_empty()
     }
+    /// Get this array as an array of ValIds
+    #[inline]
+    pub fn as_vals(&self) -> &VarArr<ValIdMarker> {
+        RefCast::ref_cast(&self.arr)
+    }
+    /// Clone this array as an array of ValIds
+    #[inline]
+    pub fn clone_vals(&self) -> VarArr<ValIdMarker> {
+        VarArr {
+            arr: self.arr.clone(),
+            variant: std::marker::PhantomData,
+        }
+    }
 }
 
-/// A marker for a ValId
+/// A marker for an array of ValIds
 #[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
 pub struct ValIdMarker;
+
+/// An array of ValIds
+pub type ValArr = VarArr<ValIdMarker>;
+
+impl ValArr {
+    /// Create a `ValArr` from (exact size) iterator over `ValId`s
+    pub fn new<I: Iterator<Item = ValId> + ExactSizeIterator>(vals: I) -> ValArr {
+        Self::dedup(ArcValIdArr(Arc::from_header_and_iter(
+            HeaderWithLength::new((), vals.len()),
+            vals,
+        )))
+    }
+    /// Deduplicate an `Arc` to an array of `ValId`s to get a `ValArr`
+    pub fn dedup(ava: ArcValIdArr) -> ValArr {
+        let dedup_ava = ARRAY_CACHE.cache(ava);
+        ValArr {
+            arr: PrivateValArr(Arc::into_thin(dedup_ava.0)),
+            variant: std::marker::PhantomData,
+        }
+    }
+}
 
 impl<V: Value> Index<usize> for VarArr<V> {
     type Output = VarId<V>;
@@ -83,29 +117,29 @@ impl Hash for PrivateValArr {
 /// A reference-counted array of values which is not necessarily hash-consed.
 /// This wrapper is for the `Hash` implementation
 #[derive(Clone, Eq, PartialEq)]
-pub struct ValArr(pub Arc<HeaderSlice<HeaderWithLength<()>, [ValId]>>);
+pub struct ArcValIdArr(pub Arc<HeaderSlice<HeaderWithLength<()>, [ValId]>>);
 
-impl Caches<ValId> for ValArr {
+impl Caches<ValId> for ArcValIdArr {
     #[inline]
     fn can_collect(&self) -> bool {
         self.0.is_unique()
     }
 }
 
-impl Deref for ValArr {
+impl Deref for ArcValIdArr {
     type Target = [ValId];
     fn deref(&self) -> &[ValId] {
         &self.0.slice
     }
 }
 
-impl Debug for ValArr {
+impl Debug for ArcValIdArr {
     fn fmt(&self, fmt: &mut Formatter) -> Result<(), fmt::Error> {
         Debug::fmt(self.deref(), fmt)
     }
 }
 
-impl Hash for ValArr {
+impl Hash for ArcValIdArr {
     fn hash<H: Hasher>(&self, hasher: &mut H) {
         self.deref().hash(hasher)
     }
