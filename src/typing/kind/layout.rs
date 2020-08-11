431eeaee462c0a7aa9d890be3b4c4db4c027e09e
Jad Ghalayini <jad.ghalayini@hotmail.com>
2020-08-11 19:22:37 -0400

    Started layouts

---
+++ src/typing/kind/layout.rs
@@ -0,0 +1,5 @@
+/*!
+Layouts of data in memory
+
+Heavily inspired by Rust's `alloc` interface, except with more support for the specification of uninitialized bytes
+*/
--- src/typing/kind/mod.rs
+++ src/typing/kind/mod.rs
@@ -4,6 +4,7 @@ Meta-types and layouts
 use super::*;
 
 pub mod universe;
+pub mod layout;
 
 /// A trait implemented by `rain` values which are a kind, i.e. a type of types
 pub trait Kind: Type {}
