2ee870d288f7d10295256ccc67852d6ea129c90b
Jad Ghalayini <jad.ghalayini@hotmail.com>
2020-08-11 19:20:32 -0400

    Predicate hierarchy

--- src/typing/kind/mod.rs
+++ src/typing/kind/mod.rs
@@ -1,10 +1,12 @@
 /*!
 Meta-types and layouts
 */
+use super::*;
+
 pub mod universe;
 
 /// A trait implemented by `rain` values which are a kind, i.e. a type of types
-pub trait Kind {}
+pub trait Kind: Type {}
 
 /// A trait implemented by `rain` values which can all be represented within a given memory layout
 pub trait Repr: Kind {}
--- src/typing/predicate.rs
+++ src/typing/predicate.rs
@@ -14,3 +14,31 @@ pub trait TypePredicate {}
 impl TypePredicate for IsType {}
 
 impl<T> TypePredicate for Is<T> where T: Type {}
+
+/// A predicate indicating a `rain` value is a kind
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash, PartialOrd, Ord)]
+pub struct IsKind;
+
+impl TypePredicate for IsKind {}
+
+/// A predicate which indicates that a `rain` value is a kind
+pub trait KindPredicate: TypePredicate {}
+
+impl KindPredicate for IsKind {}
+
+impl<K> KindPredicate for Is<K> where K: Kind {}
+
+/// A predicate indicating a `rain` value is a representation
+#[derive(Debug, Copy, Clone, Eq, PartialEq, Hash, PartialOrd, Ord)]
+pub struct IsRepr;
+
+impl TypePredicate for IsRepr {}
+
+impl KindPredicate for IsRepr {}
+
+/// A predicate which indicates that a `rain` value is a type
+pub trait ReprPredicate: KindPredicate {}
+
+impl ReprPredicate for IsRepr {}
+
+impl<R: Repr> ReprPredicate for Is<R> {}
