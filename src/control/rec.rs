f06405cb8efe30eaaabbfb7a40f10ed72e25aa80
Jad Ghalayini <jad.ghalayini@hotmail.com>
2020-08-13 18:59:24 -0400

    Recursor nodes

--- src/control/rec.rs
+++ src/control/rec.rs
@@ -6,3 +6,16 @@ The difference between a `switch` statement and a `rec` statement is that the fo
 the later is implemented with a `ValArr`. "Compression normalization" is in effect: a `switch` statement which is larger than a
 corresponding `rec` statement will normalize to the latter, and vice versa. We assume here that a pointer is 64 bits.
 */
+use crate::lifetime::Lifetime;
+use crate::value::{arr::ValArr, TypeId};
+
+/// A recursor node
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+pub struct Rec {
+    /// The type of this recursor node
+    ty: TypeId,
+    /// The lifetime of this recursor node
+    lt: Lifetime,
+    /// The branches of this recursor node
+    branches: ValArr,
+}
