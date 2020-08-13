9691eef6e44e8b08078a5b1c0cb5e868765112f9
Jad Ghalayini <jad.ghalayini@hotmail.com>
2020-08-13 19:01:30 -0400

    Switch node

--- src/control/switch.rs
+++ src/control/switch.rs
@@ -6,3 +6,16 @@ The difference between a switch statement and a `rec` statement is that the form
 the later is implemented with a `ValArr`. "Compression normalization" is in effect: a `switch` statement which is larger than a
 corresponding `rec` statement will normalize to the latter, and vice versa. We assume here that a pointer is 64 bits.
 */
+use crate::function::pi::Pi;
+use crate::lifetime::Lifetime;
+use crate::value::VarId;
+
+/// A switch node
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+pub struct Switch {
+    /// The type of this switch node
+    ty: VarId<Pi>,
+    /// The lifetime of this switch node
+    lt: Lifetime,
+    //TODO: RLE branches
+}
