3e736db2d015a59d5afc98ef01091722db5128e7
Jad Ghalayini <jad.ghalayini@hotmail.com>
2020-08-12 20:34:10 -0400

    Links

--- src/control/mod.rs
+++ src/control/mod.rs
@@ -3,13 +3,13 @@ Control flow primitives
 
 # Overview
 This module contains the following primitive elements for describing control flow in `rain` programs
-- `ternary` nodes for control flow on binary sum types such as booleans, bits, and binary sums of the form `A + B`
-- `switch` nodes for control flow on finite types
-- `rec` nodes for control flow on `n`-ary sum types and primitive recursion
-- `phi` nodes for arbitrary recursion, using the types in the `termination` module
+- [`ternary`](ternary) nodes for control flow on binary sum types such as booleans, bits, and binary sums of the form `A + B`
+- [`switch`](switch) nodes for control flow on finite types
+- [`rec`](rec) nodes for control flow on `n`-ary sum types and primitive recursion
+- [`phi`](phi) nodes for arbitrary recursion, using the types in the `termination` module
 
-The `termination` module describes a type system for encapsulating non-termination without introducing inconsistencies, and the
-`nondeterministic` module describes a similar monadic type system for encapsulating non-parametric nondeterminism, as well as
+The [`termination`](termination) module describes a type system for encapsulating non-termination without introducing inconsistencies, and the
+[`nondeterministic`](nondeterministic) module describes a similar monadic type system for encapsulating non-parametric nondeterminism, as well as
 nondeterministic control flow primitives.
 */
 
