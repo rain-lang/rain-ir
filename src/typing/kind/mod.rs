689ad7af6cf386e29590c857cdf5c4f1e385f6d1
Jad Ghalayini <jad.ghalayini@hotmail.com>
2020-08-16 18:57:37 -0400

    Universe hierarchy and traits

--- src/typing/kind/mod.rs
+++ src/typing/kind/mod.rs
@@ -3,6 +3,7 @@ Meta-types and layouts
 */
 use super::*;
 use crate::value::{KindId, ReprId, UniverseId, ValId, ValRef};
+use std::cmp::Ordering;
 
 pub mod layout;
 pub mod primitive;
@@ -41,6 +42,9 @@ pub trait Repr: Kind {
 }
 
 /// A trait implemented by `rain` values which are closed under the primitive type formers, namely Pi and Sigma
+///
+/// Universes also form a strict order, in that given two universes, one always strictly encloses the other.
+/// This *may* be relaxed to a lattice property in later versions
 pub trait Universe: Kind {
     /// Convert this representation into a `UniverseId`
     ///
@@ -50,6 +54,8 @@ pub trait Universe: Kind {
     fn into_universe(self) -> UniverseId {
         self.into_val().coerce()
     }
+    /// Compare two universes
+    fn universe_cmp(&self, other: &UniverseId) -> Ordering;
 }
 
 impl<K: KindPredicate> Kind for ValId<K> {
@@ -74,6 +80,32 @@ impl<'a, K: KindPredicate> Kind for ValRef<'a, K> {
             ValueEnum::Sexpr(s) => unimplemented!("Sexpr kinds for {}", s),
             ValueEnum::Parameter(p) => unimplemented!("Parameter kinds for {}", p),
             v => panic!("{} is not a kind!", v),
+        }
+    }
+}
+
+impl<U: UniversePredicate> Universe for ValId<U> {
+    /// Compare two universes
+    #[inline]
+    fn universe_cmp(&self, other: &UniverseId) -> Ordering {
+        match self.as_enum() {
+            ValueEnum::Prop(p) => p.universe_cmp(other),
+            ValueEnum::Fin(f) => f.universe_cmp(other),
+            ValueEnum::Set(s) => s.universe_cmp(other),
+            v => panic!("Value {} asserted to be a universe, but is not!", v)
+        }
+    }
+}
+
+impl<'a, U: UniversePredicate> Universe for ValRef<'a, U> {
+    /// Compare two universes
+    #[inline]
+    fn universe_cmp(&self, other: &UniverseId) -> Ordering {
+        match self.as_enum() {
+            ValueEnum::Prop(p) => p.universe_cmp(other),
+            ValueEnum::Fin(f) => f.universe_cmp(other),
+            ValueEnum::Set(s) => s.universe_cmp(other),
+            v => panic!("Value {} asserted to be a universe, but is not!", v)
         }
     }
 }
