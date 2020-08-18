e4b56f86a686cf3a2f6ffc6de9d46d93da660f5d
Jad Ghalayini <jad.ghalayini@hotmail.com>
2020-08-18 19:56:45 -0400

    Backlinks

--- src/lifetime/mod.rs
+++ src/lifetime/mod.rs
@@ -1,14 +1,14 @@
 /*!
 The `rain` lifetime system
 */
-use crate::value::{ValId, ValRef};
+use crate::value::{Error, ValAddr, ValId, ValRef};
 use fxhash::FxHashMap as HashMap;
 
 /// A table of backlinks, recording when values in a region are borrowed and consumed
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub struct BackTable {
     /// A map of `ValId` addresses to associated backlinks
-    backlinks: HashMap<usize, Backlinks>,
+    backlinks: HashMap<ValAddr, Backlinks>,
 }
 
 /// A set of backlinks for a given `ValId`
@@ -18,6 +18,57 @@ pub struct Backlinks {
     consumer: Option<Consumer>,
     /// The borrowers of this `ValId`, if any
     borrowers: Vec<ValId>,
+}
+
+impl Default for Backlinks {
+    fn default() -> Backlinks {
+        Backlinks::new()
+    }
+}
+
+impl Backlinks {
+    /// Create a new, empty set of backlinks. Does not allocate.
+    #[inline]
+    pub fn new() -> Backlinks {
+        Self::with_capacity(0)
+    }
+    /// Create a new set of backlinks, with a given borrower capacity
+    #[inline]
+    pub fn with_capacity(borrow_cap: usize) -> Backlinks {
+        Backlinks {
+            consumer: None,
+            borrowers: Vec::with_capacity(borrow_cap),
+        }
+    }
+    /// Sort the borrowers of this `ValId`
+    #[inline]
+    pub fn sort_borrowers(&mut self) {
+        self.borrowers.sort_by_key(ValId::as_addr);
+        self.borrowers.dedup()
+    }
+    /// Register a consumer of this `ValId`
+    ///
+    /// Return an error on conflict
+    #[inline]
+    pub fn register_consumer(&mut self, consumer: Consumer) -> Result<(), Error> {
+        if self.consumer.is_none() {
+            //TODO: field resolution, etc.
+            self.consumer = Some(consumer);
+            Ok(())
+        } else {
+            Err(Error::AffineUsed)
+        }
+    }
+    /// Register a borrower of this `ValId`
+    #[inline]
+    pub fn register_borrower(&mut self, borrower: ValId) {
+        self.borrowers.push(borrower)
+    }
+    /// Get the borrowers of this `ValId`
+    #[inline]
+    pub fn borrowers(&self) -> &[ValId] {
+        &self.borrowers[..]
+    }
 }
 
 /// A consumer for a `ValId`
