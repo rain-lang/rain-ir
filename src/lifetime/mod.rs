35053bb112f5da71e39a985c6561b5bf327ca1c0
Jad Ghalayini <jad.ghalayini@hotmail.com>
2020-08-18 20:27:14 -0400

    Implemented backlink table

--- src/lifetime/mod.rs
+++ src/lifetime/mod.rs
@@ -11,13 +11,37 @@ pub struct BackTable {
     backlinks: HashMap<ValAddr, Backlinks>,
 }
 
+impl BackTable {
+    /// Validate and sort this table's backlinks. Return an error if this is impossible
+    pub fn validate(&mut self) -> Result<(), Error> {
+        for backlink in self.backlinks.values_mut() {
+            backlink.validate()?
+        }
+        Ok(())
+    }
+    /// Register a consumer for a given address
+    pub fn register_consumer(&mut self, addr: ValAddr, consumer: Consumer) -> Result<(), Error> {
+        self.backlinks
+            .entry(addr)
+            .or_default()
+            .register_consumer(consumer)
+    }
+    /// Register a borrow for a given address
+    pub fn register_borrow(&mut self, addr: ValAddr, borrow: ValId) {
+        self.backlinks
+            .entry(addr)
+            .or_default()
+            .register_borrow(borrow)
+    }
+}
+
 /// A set of backlinks for a given `ValId`
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub struct Backlinks {
     /// The consumer of this `ValId`, if any
     consumer: Option<Consumer>,
     /// The borrowers of this `ValId`, if any
-    borrowers: Vec<ValId>,
+    borrows: Vec<ValId>,
 }
 
 impl Default for Backlinks {
@@ -37,19 +61,18 @@ impl Backlinks {
     pub fn with_capacity(borrow_cap: usize) -> Backlinks {
         Backlinks {
             consumer: None,
-            borrowers: Vec::with_capacity(borrow_cap),
+            borrows: Vec::with_capacity(borrow_cap),
         }
     }
     /// Sort the borrowers of this `ValId`
     #[inline]
-    pub fn sort_borrowers(&mut self) {
-        self.borrowers.sort_by_key(ValId::as_addr);
-        self.borrowers.dedup()
+    pub fn sort_borrows(&mut self) {
+        self.borrows.sort_by_key(ValId::as_addr);
+        self.borrows.dedup()
     }
     /// Register a consumer of this `ValId`
     ///
     /// Return an error on conflict
-    #[inline]
     pub fn register_consumer(&mut self, consumer: Consumer) -> Result<(), Error> {
         if self.consumer.is_none() {
             //TODO: field resolution, etc.
@@ -59,15 +82,35 @@ impl Backlinks {
             Err(Error::AffineUsed)
         }
     }
-    /// Register a borrower of this `ValId`
+    /// Register a borrow of this `ValId`
     #[inline]
-    pub fn register_borrower(&mut self, borrower: ValId) {
-        self.borrowers.push(borrower)
+    pub fn register_borrow(&mut self, borrower: ValId) {
+        self.borrows.push(borrower)
     }
-    /// Get the borrowers of this `ValId`
+    /// Get the borrows of this `ValId`
     #[inline]
-    pub fn borrowers(&self) -> &[ValId] {
-        &self.borrowers[..]
+    pub fn borrows(&self) -> &[ValId] {
+        &self.borrows[..]
+    }
+    /// Validate the consumer of this `ValId` does not conflict with it's borrows, sorting it's borrow array
+    ///
+    /// Return an error on failed validation
+    pub fn validate(&mut self) -> Result<(), Error> {
+        self.sort_borrows();
+        self.validate_borrows()
+    }
+    /// Validate the borrows of this `ValId`.
+    ///
+    /// # Correctness
+    /// This assumes the borrow array is sorted: if it is not, calling this function is a logic error.
+    /// Note this does *not* necessarily assume the borrow array is de-duplicated, though it generally
+    /// should be.
+    pub fn validate_borrows(&self) -> Result<(), Error> {
+        if let Some(consumer) = &self.consumer {
+            consumer.validate_borrows(self.borrows())
+        } else {
+            Ok(())
+        }
     }
 }
 
@@ -79,12 +122,24 @@ pub struct Consumer(ConsumerEnum);
 
 impl Consumer {
     /// Borrow the consumer for this `ValId`
+    #[inline]
     pub fn borrow_consumer(&self) -> ConsumerRef {
         self.0.borrow_consumer()
     }
     /// Get the consumer for this `ValId`
+    #[inline]
     pub fn into_consumer(self) -> ConsumerEnum {
         self.0
+    }
+    /// Validate a sorted array of borrows is compatible with this consumer
+    ///
+    /// # Correctness
+    /// This assumes the borrow array is sorted: if it is not, calling this function is a logic error.
+    /// Note this does *not* necessarily assume the borrow array is de-duplicated, though it generally
+    /// should be.
+    #[inline]
+    pub fn validate_borrows(&self, borrows: &[ValId]) -> Result<(), Error> {
+        self.0.validate_borrows(borrows)
     }
 }
 
@@ -108,6 +163,18 @@ impl ConsumerRef<'_> {
     pub fn clone_consumer(&self) -> Consumer {
         Consumer(self.clone_enum())
     }
+    /// Validate a sorted array of borrows is compatible with this consumer
+    ///
+    /// # Correctness
+    /// This assumes the borrow array is sorted: if it is not, calling this function is a logic error.
+    /// Note this does *not* necessarily assume the borrow array is de-duplicated, though it generally
+    /// should be.
+    #[inline]
+    pub fn validate_borrows(&self, borrows: &[ValId]) -> Result<(), Error> {
+        match self {
+            ConsumerRef::Owned(v) => ConsumerEnum::validate_owned_consumer(v.as_valid(), borrows),
+        }
+    }
 }
 
 /// An owned consumer
@@ -123,6 +190,30 @@ impl ConsumerEnum {
     pub fn borrow_consumer(&self) -> ConsumerRef {
         match self {
             ConsumerEnum::Owned(o) => ConsumerRef::Owned(o.borrow_val()),
+        }
+    }
+    /// Validate an owned consumer
+    #[inline]
+    pub fn validate_owned_consumer(owned: &ValId, borrows: &[ValId]) -> Result<(), Error> {
+        if borrows
+            .binary_search_by_key(&owned.as_addr(), ValId::as_addr)
+            .is_ok()
+        {
+            Err(Error::BorrowUsed)
+        } else {
+            Ok(())
+        }
+    }
+    /// Validate a sorted array of borrows is compatible with this consumer
+    ///
+    /// # Correctness
+    /// This assumes the borrow array is sorted: if it is not, calling this function is a logic error.
+    /// Note this does *not* necessarily assume the borrow array is de-duplicated, though it generally
+    /// should be.
+    #[inline]
+    pub fn validate_borrows(&self, borrows: &[ValId]) -> Result<(), Error> {
+        match self {
+            ConsumerEnum::Owned(v) => ConsumerEnum::validate_owned_consumer(v, borrows),
         }
     }
 }
