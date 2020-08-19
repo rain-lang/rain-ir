134fdbd584ad51ec4006ff286d09c65f04f02f7f
Jad Ghalayini <jad.ghalayini@hotmail.com>
2020-08-19 19:35:10 -0400

    Consumer enum

--- src/lifetime/mod.rs
+++ src/lifetime/mod.rs
@@ -13,4 +13,16 @@ pub struct BorrowGraph {
 /// A node in the borrow-checking graph
 ///
 /// Each one of these represents a value of an affine type, by virtue of either owning or borrowing a resource
-pub struct Node {}
+pub struct Node {
+    /// The consumer of this node in the graph
+    /// 
+    /// This can be a single owner, consuming an entire node, or a different owner for each field
+    consumer: Consumer
+}
+
+/// A consumer for a node in the borrow-checking graph
+pub enum Consumer {
+    /// A single owner consuming an entire node
+    Owner(ValId),
+    //TODO: rest
+}
