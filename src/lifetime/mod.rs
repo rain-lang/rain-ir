9453d7ff2c389eb47cfa702759937a5a0f1d9cb7
Jad Ghalayini <jad.ghalayini@hotmail.com>
2020-08-19 19:31:51 -0400

    HashMap, BorrowGraph

--- src/lifetime/mod.rs
+++ src/lifetime/mod.rs
@@ -5,13 +5,12 @@ use crate::value::{Error, ValAddr, ValId, ValRef};
 use fxhash::FxHashMap as HashMap;
 
 /// A data structure tracking ownerships and borrowing within a single parametrized value
-pub struct OwnerTable {
-
+pub struct BorrowGraph {
+    /// The nodes making up this graph
+    nodes: HashMap<ValAddr, Node>,
 }
 
-/// A node in the ownership graph
-/// 
+/// A node in the borrow-checking graph
+///
 /// Each one of these represents a value of an affine type, by virtue of either owning or borrowing a resource
-pub struct Node {
-
-}
+pub struct Node {}
