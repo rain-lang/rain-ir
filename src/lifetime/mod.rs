2d85b2b6a6f39566702d567060c0d0c307e1f192
Jad Ghalayini <jad.ghalayini@hotmail.com>
2020-08-19 19:28:55 -0400

    Tap

--- src/lifetime/mod.rs
+++ src/lifetime/mod.rs
@@ -3,3 +3,8 @@ The `rain` lifetime system
 */
 use crate::value::{Error, ValAddr, ValId, ValRef};
 use fxhash::FxHashMap as HashMap;
+
+/// A data structure tracking ownerships and borrowing within a single parametrized value
+pub struct OwnerTable {
+
+}
