9a0ff986012959097c97850fe717389c96f8f17d
Jad Ghalayini <jad.ghalayini@hotmail.com>
2020-08-19 19:35:45 -0400

    Derives

--- src/lifetime/mod.rs
+++ src/lifetime/mod.rs
@@ -5,6 +5,7 @@ use crate::value::{Error, ValAddr, ValId, ValRef};
 use fxhash::FxHashMap as HashMap;
 
 /// A data structure tracking ownerships and borrowing within a single parametrized value
+#[derive(Debug, Clone, Eq, PartialEq)]
 pub struct BorrowGraph {
     /// The nodes making up this graph
     nodes: HashMap<ValAddr, Node>,
@@ -13,6 +14,7 @@ pub struct BorrowGraph {
 /// A node in the borrow-checking graph
 ///
 /// Each one of these represents a value of an affine type, by virtue of either owning or borrowing a resource
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
 pub struct Node {
     /// The consumer of this node in the graph
     /// 
@@ -21,6 +23,7 @@ pub struct Node {
 }
 
 /// A consumer for a node in the borrow-checking graph
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
 pub enum Consumer {
     /// A single owner consuming an entire node
     Owner(ValId),
