eda2f6e5ab311598bf1e320c1ed6ab3154ccf913
Qingyuan Qie <will.qie@mail.utoronto.ca>
2020-08-23 22:46:39 -0400

    Finish implement Add

--- src/primitive/bits.rs
+++ src/primitive/bits.rs
@@ -1,23 +1,21 @@
 /*!
 Finite-valued types
 */
-use crate::eval::Apply;
-use crate::function::{pi::Pi};
-use crate::region::{Regional, Region};
+use crate::eval::{Application, Apply, EvalCtx};
+use crate::function::pi::Pi;
+use crate::region::{Region, Regional};
 use crate::typing::{
-    primitive::{FIN, Fin}, Type, Typed, Universe
+    primitive::{Fin, FIN},
+    Type, Typed, Universe,
 };
 use crate::value::{
-    NormalValue, TypeRef, ValId, Value, ValueEnum, VarId, Error, VarRef, 
-    ValueData, TypeId
+    Error, NormalValue, TypeId, TypeRef, ValId, Value, ValueData, ValueEnum, VarId, VarRef,
 };
-use crate::{
-    debug_from_display, enum_convert, quick_pretty, trivial_substitute, tyarr
-};
+use crate::{debug_from_display, enum_convert, quick_pretty, trivial_substitute, tyarr};
 use num::ToPrimitive;
 use ref_cast::RefCast;
-use std::ops::Deref;
 use std::convert::TryInto;
+use std::ops::Deref;
 
 /// A type with `n` values
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, RefCast)]
@@ -81,7 +79,7 @@ impl Value for BitsTy {
     }
     #[inline]
     fn into_norm(self) -> NormalValue {
-         NormalValue::assert_normal(ValueEnum::BitsTy(self))
+        NormalValue::assert_normal(ValueEnum::BitsTy(self))
     }
 }
 
@@ -109,17 +107,19 @@ pub struct Bits {
     ty: VarId<BitsTy>,
     /// The data this bit set stores
     data: u128,
+    /// The length of this bitvector
+    len: u32,
 }
 
 impl Bits {
     /// Try to construct a new bitset. Return an error if high bits are set.
     pub fn try_new<B: Into<VarId<BitsTy>>>(ty: B, data: u128) -> Result<Bits, Error> {
         let ty: VarId<BitsTy> = ty.into();
-        let width: u32 = ty.deref().0.try_into().map_err(|_| Error::TooManyBits)?;
-        if width > 128 || data.wrapping_shr(width.min(127)) != 0 {
+        let len: u32 = ty.deref().0.try_into().map_err(|_| Error::TooManyBits)?;
+        if len > 128 || data.wrapping_shr(len.min(127)) != 0 {
             Err(Error::TooManyBits)
         } else {
-            Ok(Bits { ty, data })
+            Ok(Bits { ty, data, len })
         }
     }
     /// Get this data
@@ -130,8 +130,7 @@ impl Bits {
     pub fn get_ty(&self) -> VarRef<BitsTy> {
         self.ty.borrow_var()
     }
-} 
-
+}
 
 debug_from_display!(Bits);
 quick_pretty!(Bits, "Unimplemented!");
@@ -145,7 +144,7 @@ enum_convert! {
 impl Typed for Bits {
     #[inline]
     fn ty(&self) -> TypeRef {
-        FIN.borrow_ty()
+        self.ty.borrow_ty()
     }
 
     #[inline]
@@ -192,7 +191,7 @@ pub struct Add {
     /// Type of the add operator
     ty: VarId<Pi>,
     /// The length of the bit vector,
-    len: u32
+    len: u32,
 }
 debug_from_display!(Add);
 quick_pretty!(Add, "Add(Need to change this)");
@@ -212,24 +211,72 @@ impl From<Add> for NormalValue {
 impl Regional for Add {}
 
 impl Apply for Add {
-    // fn apply_in<'a>(
-    //     &self,
-    //     args: &'a [ValId],
-    //     ctx: &mut Option<EvalCtx>,
-    // ) -> Result<Application<'a>, Error> {
-    //     Logical::from(*self).apply_in(args, ctx)
-    // }
+    fn apply_in<'a>(
+        &self,
+        args: &'a [ValId],
+        ctx: &mut Option<EvalCtx>,
+    ) -> Result<Application<'a>, Error> {
+        if args.len() <= 1 {
+            self.ty.apply_ty_in(args, ctx).map(Application::Symbolic)
+        } else if args.len() > 2 {
+            Err(Error::TooManyArgs)
+        } else {
+            let arg_0 = &args[0];
+            let arg_1 = &args[1];
+            match (arg_0.as_enum(), arg_1.as_enum()) {
+                (ValueEnum::Bits(left), ValueEnum::Bits(right)) => {
+                    if left.len != right.len || left.len != self.len {
+                        return Err(Error::TypeMismatch);
+                    }
+                    let result = Bits {
+                        ty: left.ty.clone(),
+                        data: left.data.wrapping_add(right.data),
+                        len: left.len,
+                    };
+                    Ok(Application::Success(&[], result.into_val()))
+                }
+                (ValueEnum::Bits(zero), x) if zero.data == 0 => {
+                    if zero.len != self.len || zero.ty != x.ty() {
+                        return Err(Error::TypeMismatch);
+                    }
+                    Ok(Application::Success(&[], args[0].clone()))
+                }
+                (x, ValueEnum::Bits(zero)) if zero.data == 0 => {
+                    if zero.len != self.len || zero.ty != x.ty() {
+                        return Err(Error::TypeMismatch);
+                    }
+                    Ok(Application::Success(&[], args[1].clone()))
+                }
+                (left, right) => {
+                    let left_ty = left.ty();
+                    if left_ty != right.ty() {
+                        // Error
+                        return Err(Error::TypeMismatch);
+                    }
+                    match left {
+                        ValueEnum::BitsTy(b) if b.0 != self.len => {
+                            Ok(Application::Symbolic(left_ty.clone_ty()))
+                        }
+                        _ => Err(Error::TypeMismatch),
+                    }
+                }
+            }
+        }
+    }
 }
 
 impl Add {
     /// Try to initilize an add operator for #bit(len)
     pub fn new(len: u32) -> Add {
-        let region = Region::with_unchecked(tyarr![BitsTy{0: len}.into_ty(); 2], Region::NULL, Fin.into_universe());
-        let pi = Pi::try_new(BitsTy{0: len}.into_ty(), region).unwrap().into_var();
-        Add {
-            ty: pi,
-            len: len
-        }
+        let region = Region::with_unchecked(
+            tyarr![BitsTy{0: len}.into_ty(); 2],
+            Region::NULL,
+            Fin.into_universe(),
+        );
+        let pi = Pi::try_new(BitsTy { 0: len }.into_ty(), region)
+            .unwrap()
+            .into_var();
+        Add { ty: pi, len: len }
     }
 }
 
