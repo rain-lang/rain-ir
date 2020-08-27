6eb8a14bc4047abb9361b1396d204dc444252a8a
Jad Ghalayini <jad.ghalayini@hotmail.com>
2020-08-27 19:49:38 -0400

    Rewrote subtraction like Add

--- src/primitive/bits/sub.rs
+++ src/primitive/bits/sub.rs
@@ -1,69 +1,22 @@
 use super::*;
 
+lazy_static! {
+    /// The subtraction operator constant
+    static ref ADD: VarId<Sub> = VarId::direct_new(Sub);
+}
+
 /// The subtraction operator
 #[derive(Clone, Eq, PartialEq, Hash)]
-pub struct Sub {
-    /// Type of the subtraction operator
-    ty: VarId<Pi>,
-    /// The length of the bit vector,
-    len: u32,
-}
+pub struct Sub;
 
-#[allow(clippy::len_without_is_empty)]
-impl Sub {
-    /// Create an subtraction operator with bitwidth `len`
-    pub fn new(len: u32) -> Sub {
-        Sub {
-            ty: Self::compute_ty(len).into_var(),
-            len,
-        }
-    }
-    /// Get the pi type of the subtraction operator with bitwidth `len`
-    ///
-    /// Note that the result of this method called on `len` is always equal to the type of `Sub::new(len)`.
-    pub fn compute_ty(len: u32) -> Pi {
-        let region = Region::with_unchecked(
-            tyarr![BitsTy{0: len}.into_ty(); 2],
-            Region::NULL,
-            Fin.into_universe(),
-        );
-        Pi::try_new(BitsTy(len).into_ty(), region)
-            .expect("The type of the multiply operator is always valid")
-    }
-    /// Perform wrapping bitvector subtraction, discarding high order bits
-    ///
-    /// This method assumes both `left` and `right` are valid bitvectors for this subtraction operation, namely that they have length
-    /// less than or equal to `self.len()`. If this is not the case, this function will panic *in debug mode*, while in release mode,
-    /// the behaviour is unspecified but safe.
-    #[inline(always)]
-    pub fn masked_sub(&self, left: u128, right: u128) -> u128 {
-        debug_assert_eq!(
-            left,
-            mask(self.len, left),
-            "Left bitvector has length greater than len"
-        );
-        debug_assert_eq!(
-            right,
-            mask(self.len, right),
-            "Right bitvector has length greater than len"
-        );
-        masked_sub(self.len, left, right)
-    }
-    /// Get the bitwidth of this subtraction operator
-    #[inline(always)]
-    pub fn len(&self) -> u32 {
-        self.len
-    }
-}
-
-/// Perform wrapping bitvector multiplication, discarding bits of order greater than `len`
+/// Perform wrapping bitvector subtraction, discarding bits of order greater than `len`
 #[inline(always)]
 pub fn masked_sub(len: u32, left: u128, right: u128) -> u128 {
     mask(len, left.wrapping_sub(right))
 }
 
 debug_from_display!(Sub);
-quick_pretty!(Sub, "Mul(Need to change this)");
+quick_pretty!(Sub, "Sub(Need to change this)");
 trivial_substitute!(Sub);
 enum_convert! {
     impl InjectionRef<ValueEnum> for Sub {}
@@ -85,36 +38,41 @@ impl Apply for Sub {
         args: &'a [ValId],
         ctx: &mut Option<EvalCtx>,
     ) -> Result<Application<'a>, Error> {
-        if args.len() <= 1 {
-            self.ty.apply_ty_in(args, ctx).map(Application::Symbolic)
-        } else if args.len() > 2 {
+        if args.len() <= 2 {
+            BITS_BINARY.apply_ty_in(args, ctx).map(Application::Symbolic)
+        } else if args.len() > 3 {
             Err(Error::TooManyArgs)
         } else {
-            let arg_0 = &args[0];
-            let arg_1 = &args[1];
-            match (arg_0.as_enum(), arg_1.as_enum()) {
-                (ValueEnum::Bits(left), ValueEnum::Bits(right)) => {
-                    if left.len != right.len || left.len != self.len {
+            match (args[0].as_enum(), args[1].as_enum(), args[2].as_enum()) {
+                (ValueEnum::BitsTy(ty), ValueEnum::Bits(left), ValueEnum::Bits(right)) => {
+                    if left.len != right.len || left.len != ty.0 {
                         return Err(Error::TypeMismatch);
                     }
                     let result = Bits {
                         ty: left.ty.clone(),
-                        data: self.masked_sub(left.data, right.data),
+                        data: masked_sub(ty.0, left.data, right.data),
                         len: left.len,
                     };
                     Ok(Application::Success(&[], result.into_val()))
                 }
-                (left, right) => {
-                    let left_ty = left.ty();
-                    if left_ty != right.ty() {
-                        // Error
+                (ValueEnum::BitsTy(ty), ValueEnum::Bits(zero), x) if zero.data == 0 => {
+                    if zero.len != ty.0 || zero.ty != x.ty() {
                         return Err(Error::TypeMismatch);
                     }
-                    match left {
-                        ValueEnum::BitsTy(b) if b.0 != self.len => {
-                            Ok(Application::Symbolic(left_ty.clone_as_ty()))
-                        }
-                        _ => Err(Error::TypeMismatch),
+                    Ok(Application::Success(&[], args[0].clone()))
+                }
+                (ValueEnum::BitsTy(ty), x, ValueEnum::Bits(zero)) if zero.data == 0 => {
+                    if zero.len != ty.0 || zero.ty != x.ty() {
+                        return Err(Error::TypeMismatch);
+                    }
+                    Ok(Application::Success(&[], args[1].clone()))
+                }
+                (ty, left, right) => {
+                    let left_ty = left.ty();
+                    if left_ty != right.ty() || left_ty != args[0] || ty.ty() != *BITS_KIND {
+                        Err(Error::TypeMismatch)
+                    } else {
+                        Ok(Application::Symbolic(args[0].clone().coerce()))
                     }
                 }
             }
@@ -125,7 +83,7 @@ impl Apply for Sub {
 impl Typed for Sub {
     #[inline]
     fn ty(&self) -> TypeRef {
-        self.ty.borrow_ty()
+        BITS_BINARY.borrow_ty()
     }
     #[inline]
     fn is_ty(&self) -> bool {
@@ -154,7 +112,7 @@ impl Value for Sub {
     }
     fn get_dep(&self, ix: usize) -> &ValId {
         panic!(
-            "Add operation {} has no dependencies (tried to get dep #{})",
+            "Sub operation {} has no dependencies (tried to get dep #{})",
             self, ix
         )
     }
@@ -169,7 +127,6 @@ impl Value for Sub {
 }
 
 impl ValueData for Sub {}
-
 
 #[cfg(test)]
 mod tests {
@@ -187,19 +144,22 @@ mod tests {
             (8, 253, 255, 254),
         ];
         for (len, left, right, result) in test_cases.iter() {
-            let left_data = BitsTy(*len).data(*left).expect("Left data is valid");
-            let right_data = BitsTy(*len).data(*right).expect("Right data is valid");
-            let subtract_struct = Sub::new(*len);
-            let data_arr = [left_data.into_val(), right_data.into_val()];
+            let bitwidth = BitsTy(*len).into_var();
+            let left_data = bitwidth.data(*left).expect("Left data is valid");
+            let right_data = bitwidth.data(*right).expect("Right data is valid");
+            let data_arr = [
+                bitwidth.into_val(),
+                left_data.into_val(),
+                right_data.into_val(),
+            ];
             let mut ctx = None;
-            match subtract_struct.apply_in(&data_arr[..], &mut ctx).unwrap() {
+            match Sub.apply_in(&data_arr[..], &mut ctx).unwrap() {
                 Application::Success(&[], v) => match v.as_enum() {
                     ValueEnum::Bits(b) => {
                         assert_eq!(b.len, *len);
                         assert_eq!(b.data, *result);
                         assert_eq!(b.data, mask(*len, left.wrapping_sub(*right)));
                         assert_eq!(b.data, masked_sub(*len, *left, *right));
-                        assert_eq!(b.data, subtract_struct.masked_sub(*left, *right));
                     }
                     _ => panic!("Result should be a bitvector constant (ValueEnum::Bits)"),
                 },
@@ -207,7 +167,7 @@ mod tests {
             };
             assert_eq!(
                 ctx, None,
-                "No evaluation context should be generated by direct addition of constants"
+                "No evaluation context should be generated by direct subtraction of constants"
             );
         }
     }
