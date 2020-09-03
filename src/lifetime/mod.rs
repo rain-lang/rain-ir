b342916cb3d3837c824dfb26db5755aad88a6e5f
Jad Ghalayini <jad.ghalayini@hotmail.com>
2020-09-03 12:58:09 -0400

    Lifetime construction

--- src/lifetime/mod.rs
+++ src/lifetime/mod.rs
@@ -2,10 +2,11 @@
 The `rain` lifetime system
 */
 
-use crate::region::data::RegionData;
+use crate::region::{data::RegionData, Region};
 use crate::value::Error;
 use crate::value::{ValId, ValRef};
 use elysees::Arc;
+use elysees::UnionAlign;
 use erasable::ErasedPtr;
 use fxhash::FxBuildHasher;
 use indexmap::{map::Entry, IndexMap};
@@ -29,6 +30,27 @@ pub use data::*;
 #[derive(Debug, Clone, Eq, Default)]
 pub struct Lifetime(Option<Union2<Arc<RegionData>, Arc<LifetimeData>>>);
 
+impl Lifetime {
+    /// The static `rain` lifetime
+    pub const STATIC: Lifetime = Lifetime(None);
+    /// Get a pointer to the data underlying this lifetime
+    #[inline]
+    pub fn as_ptr(&self) -> Option<ErasedPtr> {
+        self.0.as_ref().map(|ptr| ptr.as_untagged_ptr())
+    }
+}
+
+impl From<Region> for Lifetime {
+    #[inline]
+    fn from(region: Region) -> Lifetime {
+        if let Some(arc) = region.into_arc() {
+            Lifetime(Some(UnionAlign::left(arc)))
+        } else {
+            Lifetime::STATIC
+        }
+    }
+}
+
 impl PartialEq for Lifetime {
     #[inline]
     fn eq(&self, other: &Lifetime) -> bool {
@@ -40,15 +62,5 @@ impl Hash for Lifetime {
     #[inline]
     fn hash<H: Hasher>(&self, hasher: &mut H) {
         self.as_ptr().hash(hasher)
-    }
-}
-
-impl Lifetime {
-    /// The static `rain` lifetime
-    pub const STATIC: Lifetime = Lifetime(None);
-    /// Get a pointer to the data underlying this lifetime
-    #[inline]
-    pub fn as_ptr(&self) -> Option<ErasedPtr> {
-        self.0.as_ref().map(|ptr| ptr.as_untagged_ptr())
     }
 }
