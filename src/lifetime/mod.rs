f4f530db1103470126bcbc37d0c03900dec3d8e3
Jad Ghalayini <jad.ghalayini@hotmail.com>
2020-09-03 12:51:53 -0400

    Implemented PartialEq and Hash

--- src/lifetime/mod.rs
+++ src/lifetime/mod.rs
@@ -2,15 +2,17 @@
 The `rain` lifetime system
 */
 
-use crate::region::{data::RegionData};
+use crate::region::data::RegionData;
 use crate::value::Error;
 use crate::value::{ValId, ValRef};
 use elysees::Arc;
+use erasable::ErasedPtr;
 use fxhash::FxBuildHasher;
 use indexmap::{map::Entry, IndexMap};
 use itertools::{EitherOrBoth, Itertools};
 use ptr_union::Union2;
 use smallvec::SmallVec;
+use std::hash::{Hash, Hasher};
 use std::iter::Copied;
 use std::ops::Deref;
 
@@ -24,10 +26,29 @@ mod data;
 pub use data::*;
 
 /// A `rain` lifetime
-#[derive(Debug, Clone, Eq, PartialEq, Hash, Default)]
+#[derive(Debug, Clone, Eq, Default)]
 pub struct Lifetime(Option<Union2<Arc<RegionData>, Arc<LifetimeData>>>);
+
+impl PartialEq for Lifetime {
+    #[inline]
+    fn eq(&self, other: &Lifetime) -> bool {
+        self.as_ptr() == other.as_ptr()
+    }
+}
+
+impl Hash for Lifetime {
+    #[inline]
+    fn hash<H: Hasher>(&self, hasher: &mut H) {
+        self.as_ptr().hash(hasher)
+    }
+}
 
 impl Lifetime {
     /// The static `rain` lifetime
     pub const STATIC: Lifetime = Lifetime(None);
+    /// Get a pointer to the data underlying this lifetime
+    #[inline]
+    pub fn as_ptr(&self) -> Option<ErasedPtr> {
+        self.0.as_ref().map(|ptr| ptr.as_untagged_ptr())
+    }
 }
