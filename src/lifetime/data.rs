11f9cf9a89640790479741fb00427a8119c44917
Jad Ghalayini <jad.ghalayini@hotmail.com>
2020-09-03 12:05:58 -0400

    Basic lifetime pointer type

---
+++ src/lifetime/data.rs
@@ -0,0 +1,9 @@
+/*!
+Data describing a `rain` lifetime
+*/
+
+/// Data describing a nontrivial `rain` lifetime
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+pub struct LifetimeData {
+    //TODO: this
+}
--- src/lifetime/mod.rs
+++ src/lifetime/mod.rs
@@ -2,11 +2,14 @@
 The `rain` lifetime system
 */
 
+use crate::region::{data::RegionData};
 use crate::value::Error;
 use crate::value::{ValId, ValRef};
+use elysees::Arc;
 use fxhash::FxBuildHasher;
 use indexmap::{map::Entry, IndexMap};
 use itertools::{EitherOrBoth, Itertools};
+use ptr_union::Union2;
 use smallvec::SmallVec;
 use std::iter::Copied;
 use std::ops::Deref;
@@ -17,3 +20,14 @@ mod params;
 pub use params::*;
 mod ctx;
 pub use ctx::*;
+mod data;
+pub use data::*;
+
+/// A `rain` lifetime
+#[derive(Debug, Clone, Eq, PartialEq, Hash, Default)]
+pub struct Lifetime(Option<Union2<Arc<RegionData>, Arc<LifetimeData>>>);
+
+impl Lifetime {
+    /// The static `rain` lifetime
+    pub const STATIC: Lifetime = Lifetime(None);
+}
